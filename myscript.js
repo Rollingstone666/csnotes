222ff5edc7c1cd7e6454e17f36640ce7556167ff
Rollingstone666 <wangyicheng0527@gmail.com>
2022-02-19 22:13:15 +0800

    update

--- myscript.js
+++ myscript.js
@@ -5,7 +5,7 @@ const input = document.querySelector('#input')
 const input2 = document.querySelector('#input2')
 const input3 = document.querySelector('#input3')
 const password = document.querySelector('#password')
-const users =document.querySelector('#users')
+const users = document.querySelector('#users')
 const print = x => {
     appContent.innerHTML = x
 }
@@ -24,6 +24,7 @@ let formatPNPli = user => "<li>" +
     "<div class='nationality'>" + user.nationality + "</div>" +
     "<div class='password'>" + ("*").repeat(user.password.length) + "</div>"
     + "</li>"
+let format2Csv = user => user.userName + "," + user.phoneNumber + "," + user.nationality + "," + user.password
 let formatArr = (arrX) => "<ul>" + arrX.map(formatPNPli).join("") + "</ul>"
 let phoneNumbers = {
     arr: [],
@@ -31,11 +32,12 @@ let phoneNumbers = {
     sort: () => phoneNumbers.arr.sort(
         (x, y) => (x.userName > y.userName) ? 1 : -1),
     getsg: () => phoneNumbers.arr.filter(x => sgnumber(x.phoneNumber)),
-    format: () => formatArr(phoneNumbers.arr)
+    format: () => formatArr(phoneNumbers.arr),
+    formatCsv: () => phoneNumbers.arr.map(format2Csv).join("\n")
 }
 
 let filtered = false
-const buttonTrigger = () => {  
+const buttonTrigger = () => {
     let b
     if (isNumber(input2.value)) {
         let newEntry = {
@@ -78,6 +80,13 @@ const resetButtonTrigger = () => {
     phoneNumbers.arr = []
     print("<center>Entries cleared!</center>")
     users.innerHTML = "(" + phoneNumbers.arr.length + ")"
+}
+
+const downLoadButtonTrigger = () => {
+    const content = phoneNumbers.formatCsv()
+    window.open(
+        encodeURI("data:text/csv;charset=utf-8," + content)
+    )
 }
 users.innerHTML = "(" + phoneNumbers.arr.length + ")"
 //let i = 100
